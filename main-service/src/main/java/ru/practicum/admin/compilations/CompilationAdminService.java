3ccd824d5448fbcdf7d3de1f414b4d62cbf89dab
Pavel <pavel2001stepanov@yandex.ru>
2024-10-19 08:49:31 +0300

    update updateCompilation

--- main-service/src/main/java/ru/practicum/admin/compilations/CompilationAdminService.java
+++ main-service/src/main/java/ru/practicum/admin/compilations/CompilationAdminService.java
@@ -24,86 +24,92 @@ import java.util.List;
 @Slf4j
 public class CompilationAdminService {
 
-    private final CompilationRepository compilationRepository;
-    private final CompilationsEventRepository compilationsEventRepository;
-    private final EventRepository eventRepository;
-    private final CompilationMapper compilationMapper;
-    private final EventMapper eventMapper;
+	private final CompilationRepository compilationRepository;
+	private final CompilationsEventRepository compilationsEventRepository;
+	private final EventRepository eventRepository;
+	private final CompilationMapper compilationMapper;
+	private final EventMapper eventMapper;
 
-    @Transactional
-    public CompilationDto addCompilation(NewCompilationDto newCompilationDto) {
-        Compilation compilation = compilationMapper.toCompilation(newCompilationDto);
-        compilation.setPinned(Boolean.TRUE.equals(newCompilationDto.getPinned()));
-        compilationRepository.save(compilation);
+	@Transactional
+	public CompilationDto addCompilation(NewCompilationDto newCompilationDto) {
+		Compilation compilation = compilationMapper.toCompilation(newCompilationDto);
+		compilation.setPinned(Boolean.TRUE.equals(newCompilationDto.getPinned()));
+		compilationRepository.save(compilation);
 
-        if (newCompilationDto.getEvents() != null && !newCompilationDto.getEvents().isEmpty()) {
-            saveCompilationEvents(newCompilationDto.getEvents(), compilation);
-        }
+		if (newCompilationDto.getEvents() != null && !newCompilationDto.getEvents().isEmpty()) {
+			saveCompilationEvents(newCompilationDto.getEvents(), compilation);
+		}
 
-        CompilationDto compilationDto = mapToCompilationDtoWithEvents(compilation);
-        log.info("Added compilation: {}", compilationDto);
-        return compilationDto;
-    }
+		CompilationDto compilationDto = mapToCompilationDtoWithEvents(compilation);
+		log.info("Added compilation: {}", compilationDto);
+		return compilationDto;
+	}
 
-    @Transactional
-    public CompilationDto updateCompilation(Long compId, UpdateCompilationRequestDto updateCompilationRequestDto) {
-        Compilation compilation = getCompilationById(compId);
+	@Transactional
+	public CompilationDto updateCompilation(Long compId, UpdateCompilationRequestDto updateCompilationRequestDto) {
+		Compilation compilation = getCompilationById(compId);
 
-        if (updateCompilationRequestDto.getTitle() != null) {
-            compilation.setTitle(updateCompilationRequestDto.getTitle());
-        }
-        if (updateCompilationRequestDto.getPinned() != null) {
-            compilation.setPinned(updateCompilationRequestDto.getPinned());
-        }
+		updateCompilationDetails(compilation, updateCompilationRequestDto);
 
-        if (updateCompilationRequestDto.getEvents() != null) {
-            compilationsEventRepository.deleteAllByCompilationId(compId);
-            if (!updateCompilationRequestDto.getEvents().isEmpty()) {
-                saveCompilationEvents(updateCompilationRequestDto.getEvents(), compilation);
-            }
-        }
+		if (updateCompilationRequestDto.getEvents() != null) {
+			compilationsEventRepository.deleteAllByCompilationId(compId);
+			if (!updateCompilationRequestDto.getEvents().isEmpty()) {
+				saveCompilationEvents(updateCompilationRequestDto.getEvents(), compilation);
+			}
+		}
 
-        CompilationDto compilationDto = mapToCompilationDtoWithEvents(compilation);
-        log.info("Updated compilation: {}", compilationDto);
-        return compilationDto;
-    }
+		CompilationDto compilationDto = mapToCompilationDtoWithEvents(compilation);
+		log.info("Updated compilation: {}", compilationDto);
+		return compilationDto;
+	}
 
-    @Transactional
-    public void deleteCompilation(Long compId) {
-        Compilation compilation = getCompilationById(compId);
-        compilationRepository.delete(compilation);
-        compilationsEventRepository.deleteAllByCompilationId(compId);
-        log.info("Deleted compilation with id: {}", compId);
-    }
+	private void updateCompilationDetails(Compilation compilation,
+			UpdateCompilationRequestDto updateCompilationRequestDto) {
+		if (updateCompilationRequestDto.getTitle() != null) {
+			compilation.setTitle(updateCompilationRequestDto.getTitle());
+		}
+		if (updateCompilationRequestDto.getPinned() != null) {
+			compilation.setPinned(updateCompilationRequestDto.getPinned());
+		}
+	}
 
-    private void saveCompilationEvents(List<Long> eventIds, Compilation compilation) {
-        for (Long eventId : eventIds) {
-            Event event = getEventById(eventId);
-            CompilationEvent compilationEvent = new CompilationEvent();
-            compilationEvent.setCompilation(compilation);
-            compilationEvent.setEvent(event);
-            compilationsEventRepository.save(compilationEvent);
-        }
-    }
+	@Transactional
+	public void deleteCompilation(Long compId) {
+		Compilation compilation = getCompilationById(compId);
+		compilationRepository.delete(compilation);
+		compilationsEventRepository.deleteAllByCompilationId(compId);
+		log.info("Deleted compilation with id: {}", compId);
+	}
 
-    private CompilationDto mapToCompilationDtoWithEvents(Compilation compilation) {
-        List<CompilationEvent> compilationEventList = compilationsEventRepository.findAllByCompilationId(compilation.getId());
-        List<EventShortDto> events = new ArrayList<>();
-        for (CompilationEvent compilationEvent : compilationEventList) {
-            events.add(eventMapper.toEventShortDto(compilationEvent.getEvent()));
-        }
-        CompilationDto compilationDto = compilationMapper.toCompilationDto(compilation);
-        compilationDto.setEvents(events);
-        return compilationDto;
-    }
+	private void saveCompilationEvents(List<Long> eventIds, Compilation compilation) {
+		for (Long eventId : eventIds) {
+			Event event = getEventById(eventId);
+			CompilationEvent compilationEvent = new CompilationEvent();
+			compilationEvent.setCompilation(compilation);
+			compilationEvent.setEvent(event);
+			compilationsEventRepository.save(compilationEvent);
+		}
+	}
 
-    private Event getEventById(Long eventId) {
-        return eventRepository.findById(eventId)
-                .orElseThrow(() -> new NotFoundException("Event with id " + eventId + " not found."));
-    }
+	private CompilationDto mapToCompilationDtoWithEvents(Compilation compilation) {
+		List<CompilationEvent> compilationEventList = compilationsEventRepository
+				.findAllByCompilationId(compilation.getId());
+		List<EventShortDto> events = new ArrayList<>();
+		for (CompilationEvent compilationEvent : compilationEventList) {
+			events.add(eventMapper.toEventShortDto(compilationEvent.getEvent()));
+		}
+		CompilationDto compilationDto = compilationMapper.toCompilationDto(compilation);
+		compilationDto.setEvents(events);
+		return compilationDto;
+	}
 
-    private Compilation getCompilationById(Long compId) {
-        return compilationRepository.findById(compId)
-                .orElseThrow(() -> new NotFoundException("Compilation with id " + compId + " not found."));
-    }
+	private Event getEventById(Long eventId) {
+		return eventRepository.findById(eventId)
+				.orElseThrow(() -> new NotFoundException("Event with id " + eventId + " not found."));
+	}
+
+	private Compilation getCompilationById(Long compId) {
+		return compilationRepository.findById(compId)
+				.orElseThrow(() -> new NotFoundException("Compilation with id " + compId + " not found."));
+	}
 }
